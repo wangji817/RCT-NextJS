9467ee1c62b0e945f3a6df1304f792321934a0c2
wangji817 <526153861@qq.com>
2022-08-01 14:17:23 +0800

    commit

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,18 @@ import { useEffect, useState } from 'react';
 
 export default function Home() {
   const [text, setTextarea] = useState();
+  const [title, setTitle] = useState();
   const changeText = (e) => {
     console.log(e.target.value);
     setTextarea(e.target.value);
   }
+
+  useEffect(() => {
+    setTitle('我是新的标题');
+  }, [])
   return (
     <div className={styles.container}>
+      <div>{title}</div>
       <TextArea rows={4} placeholder="数据展示" showCount autoSize bordered onChange={changeText} />
       <div>{text}</div>
     </div>
